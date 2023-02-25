a817ea266083966310e10589131384491489e9ce
Rosen Toshev <rtoscheff@gmail.com>
2023-02-25 22:36:36 +0000

    Solved the chessboard using major and minor diagonals

--- js/chessboard.js
+++ js/chessboard.js
@@ -1,67 +1,53 @@
 export default {
-	draw,
-	highlight
+  draw,
+  highlight,
 };
-
 
 // ****************************
 
-var tiles;
+var diagonals = [];
+var highlighted = [];
+var tileDiagonals = new Map();
 
 function draw(boardEl) {
-	for (let i = 0; i <= 7; i++) {
-		let rowEl = document.createElement("div");
-		for (let j = 0; j <= 7; j++) {
-			let tileEl = document.createElement("div");
-			tileEl.dataset.row = i;
-			tileEl.dataset.col = j;
-			rowEl.appendChild(tileEl);
-		}
-		boardEl.appendChild(rowEl);
-	}
-	tiles = boardEl.querySelectorAll("div > div");
+  for (let i = 0; i < 30; i++) {
+    diagonals.push([]);
+  }
+
+  for (let i = 0; i <= 7; i++) {
+    let rowEl = document.createElement("div");
+    for (let j = 0; j <= 7; j++) {
+      let tileEl = document.createElement("div");
+      rowEl.appendChild(tileEl);
+
+      let majorDiag = diagonals[7 - (i - j)];
+      let minorDiag = diagonals[15 + (i + j)];
+
+      majorDiag.push(tileEl);
+      minorDiag.push(tileEl);
+
+      tileDiagonals.set(tileEl, [majorDiag, minorDiag]);
+    }
+    boardEl.appendChild(rowEl);
+  }
 }
 
 function highlight(tileEl) {
-	// clear all currently highlighted tiles (if any)
-	for (let el of tiles) {
-		el.classList.remove("highlighted");
-	}
+  // clear all currently highlighted tiles (if any)
+  for (let diagonal of highlighted) {
+    for (let el of diagonal) {
+      el.classList.remove("highlighted");
+    }
+  }
 
-	// clicked on a board tile?
-	if (tileEl) {
-		let rowEl = tileEl.parentNode;
-		let boardEl = rowEl.parentNode;
-		let tileRowIdx = [...boardEl.childNodes].findIndex(el => el == rowEl);
-		let tileColIdx = [...rowEl.childNodes].findIndex(el => el == tileEl);
+  // clicked on a board tile?
+  if (tileEl) {
+    highlighted = tileDiagonals.get(tileEl);
 
-		// highlight in up-left direction
-		for (let i = tileRowIdx, j = tileColIdx; i >= 0 && j >= 0; i--, j--) {
-			let el = findTile(i,j);
-			el.classList.add("highlighted");
-		}
-		// highlight in the up-right direction
-		for (let i = tileRowIdx, j = tileColIdx; i >= 0 && j <= 7; i--, j++) {
-			let el = findTile(i,j);
-			el.classList.add("highlighted");
-		}
-		// highlight in down-left direction
-		for (let i = tileRowIdx, j = tileColIdx; i <= 7 && j >= 0; i++, j--) {
-			let el = findTile(i,j);
-			el.classList.add("highlighted");
-		}
-		// highlight in the down-right direction
-		for (let i = tileRowIdx, j = tileColIdx; i <= 7 && j <= 7; i++, j++) {
-			let el = findTile(i,j);
-			el.classList.add("highlighted");
-		}
-	}
-}
-
-function findTile(row,col) {
-	for (let el of tiles) {
-		if (el.dataset.row == row && el.dataset.col == col) {
-			return el;
-		}
-	}
+    for (let diagonal of highlighted) {
+      for (let el of diagonal) {
+        el.classList.add("highlighted");
+      }
+    }
+  }
 }
